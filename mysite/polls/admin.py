68b8549f0a2427c02415236bfcb645ccd9d2873b
Badri Thapa <badrithapa9042@gmail.com>
2022-01-21 12:56:02 +0545

    Choice added in admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 admin.site.register(Question) # this makes the polls app modifiable
 ##this arlso tell the admin that Question objects have admin interface so that it will be displyed in admin index page
+admin.site.register(Choice) ## this tells that choice can be seen in admin index and modified there withou any command lines
 
 # Register your models here.
