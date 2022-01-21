cf0e492bf6d0cd33258f15a3fadaaacd6c47c4fa
Badri Thapa <badrithapa9042@gmail.com>
2022-01-21 12:52:43 +0545

    admin created and question obecjt reference in admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Question
+admin.site.register(Question) # this makes the polls app modifiable
+##this arlso tell the admin that Question objects have admin interface so that it will be displyed in admin index page
 
 # Register your models here.
